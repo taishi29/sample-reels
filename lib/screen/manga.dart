711d53aafddbaae6ca5c47090cc02265789e1d6c
taishi29 <115542864+taishi29@users.noreply.github.com>
2025-02-17 14:02:10 +0900

    update: #3 #4 #5 各カテゴリページに遷移できるようにする

---
+++ lib/screen/manga.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MangaPage extends StatelessWidget {
+  const MangaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("マンガページ", style: TextStyle(fontSize: 24)));
+  }
+}
---
+++ lib/screen/movie.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoviePage extends StatelessWidget {
+  const MoviePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("動画ページ", style: TextStyle(fontSize: 24)));
+  }
+}
