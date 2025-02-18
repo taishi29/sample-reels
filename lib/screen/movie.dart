d8bc8cefe053df60f0dd6f16638570d277760570
taishi29 <115542864+taishi29@users.noreply.github.com>
2025-02-19 01:45:46 +0900

    feat: 画像の埋め込みをします。

--- lib/screen/movie.dart
+++ lib/screen/movie.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:sample_reels/component/side_buttons.dart'; // いいねボタン用
+import 'package:sample_reels/component/image_slide.dart';
 
 class MoviePage extends StatefulWidget {
   const MoviePage({super.key});
@@ -15,6 +16,11 @@ class MoviePageState extends State<MoviePage> {
   late VideoPlayerController _controller;
   bool _isControllerInitialized = false;
 
+  final List<String> imageUrls = [
+    'https://i.ytimg.com/vi/_fxIVudzl2o/maxresdefault.jpg',
+    'https://jprime.ismcdn.jp/mwimgs/7/5/620mw/img_75cb49b01a58c3aad1bdb6034ad88c23709193.jpg',
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -24,7 +30,7 @@ class MoviePageState extends State<MoviePage> {
   // **動画を初期化**
   void _initializeVideo() {
     _controller = VideoPlayerController.network(
-      'https://cc3001.dmm.co.jp/litevideo/freepv/p/pre/pred00742/pred00742mhb.mp4', // ✅ ここに直接動画URLを埋め込む
+      'https://youtu.be/uAHI8mSi5fY', // ✅ ここに直接動画URLを埋め込む
     )..initialize().then((_) {
         setState(() {
           _isControllerInitialized = true;
@@ -67,7 +73,7 @@ class MoviePageState extends State<MoviePage> {
               ),
               Expanded(
                 flex: 2, // 画面の2/3を使う
-                child: Container(color: Colors.red),
+                child: ImageSlider(imageUrls: imageUrls),
               ),
             ],
           ),
